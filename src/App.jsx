ce1966c1114366fa02ebecbd6a2d1e98ea03774c
Sanaya James <sanaya.j.james@gmail.com>
2026-06-10 17:17:40 -0700

    changed import from react router to react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import ArtistPage from './pages/ArtistPage'
 import HomePage from './pages/HomePage'
