3d45a1d15ede8da874eb950d53ae80637f64d409
Sanaya James <sanaya.j.james@gmail.com>
2026-06-16 18:59:46 -0700

    added highlighted works

--- src/pages/ArtistBioPage.jsx
+++ src/pages/ArtistBioPage.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 export default function ArtistBioPage() {
   const {artistId} = useParams();
   const [artistData, setArtistData] = useState(null);
+  const [works, setWorks] = useState([]);
   const [error, setError] = useState(null);
 
   // fetch artist profile from aic api
@@ -28,12 +29,47 @@ export default function ArtistBioPage() {
       setError("Failed to load artist details");
     })
   }, [artistId])
+
+  // fetch artworks for that artist => Highlighted Works
+  useEffect(() => {
+    if (!artistData) return;
+
+    fetch(`${settings.met.baseurl}/search?q=${encodeURIComponent(artistData.title)}`)
+      .then((r) => {
+        if (!r.ok) { throw new Error("Failed to fetch.") }
+        return r.json()
+      })
+      .then(data => {
+        console.log(data);
+        const ids = data.objectIDs?.slice(0, 12) || [];
+
+        // must use second fetch bc first returns only objectIDs
+        return Promise.all(
+          ids.map(id =>
+            fetch(`${settings.met.baseurl}/objects/${id}`)
+              .then(r => {
+                if (!r.ok) { throw new Error("Failed to fetch") }
+                return r.json();
+              })
+          )
+        );
+      })
+      .then((results) => {
+        console.log(results)
+        const filteredWorks = results.filter(
+          work =>
+            work.artistDisplayName?.toLowerCase() === artistData.title.toLowerCase()
+        )
+
+        setWorks(filteredWorks)
+      })
+      .catch(console.error)
+  }, [artistData])
   
   if (error) {
     return <p>{error}</p>
   }
   
-  // doesn't work. if api fails, this stays on the screen. implement better error handling
   if (!artistId) {
     return <p>Loading artist information ...</p>;
   }
@@ -41,13 +77,15 @@ export default function ArtistBioPage() {
     return <p>Loading artist details ...</p>
   }
   
-  // fetch artworks for that artist => Highlighted Works
+  // if (!works) {
+  //   return <p>Loading artist details ...</p>
+  // }
 
   return (
     <div>
       {/* // first half will be from AIC api: the name, date, portrait, bio, etc. */}
       <h1>{artistData.title}</h1>
-      {/* <p>{artist.nationality}</p> */}
+      {/* <p>{artistData.nationality}</p> */}
       <p>{artistData.birth_date} - {artistData.death_date}</p>
       <div className="artist-description">
         <div dangerouslySetInnerHTML={{
@@ -58,13 +96,18 @@ export default function ArtistBioPage() {
       {/* // second half (highlighted works) will be from Met api */}
       <h2>Highlighted Works</h2>
 
-      {/* {artist.artworks.map(work => (
-        <div key={work.objectID}>
-          <img src={work.primaryImageSmall} />
-          <p>{work.title}</p>
-          <p>{work.objectDate}</p>
-        </div>
-      ))} */}
+      <div className="worksGrid">
+        {works.map(work => (
+          <div key={work.objectID} className="workCard">
+            <img
+              src={work.primaryImageSmall}
+              alt={work.title}
+            />
+            <h3>{work.title}</h3>
+            <p>{work.objectDate}</p>
+          </div>
+        ))}
+      </div>
 
     </div>
   )
